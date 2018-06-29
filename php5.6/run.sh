18843538270591c899ceb75d6a24c96409948260
William Yao <wmyao@emotibot.com.cn>
2018-06-29 11:10:09 +0800

    php5.6 apache2.4.33 ubuntu

---
+++ php5.6/run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+docker rm -fv httpd2
+#docker run -it -p 80:80 \
+docker run -d -p 80:80 \
+    --name httpd2 \
+    -v /home/wmyao/workspaces/phpbase/sh/phpbase/html:/var/www/html \
+docker-reg.emotibot.com.cn:55688/php5.6:apache2.4.33-ubuntu 
+#docker-reg.emotibot.com.cn:55688/php5.6:apache2.4.33-1
+
+
+sleep 1
+
+curl localhost/curl-check.php
+
+#docker-reg.emotibot.com.cn:55688/php5.6:apache2.4.33-1
+#docker-reg.emotibot.com.cn:55688/php7.1:apache2.4.33
